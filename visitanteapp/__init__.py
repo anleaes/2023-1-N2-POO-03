a209130d981fcc3f218f7b46c2702a1ee30d8e27
HiroshiAT <91483406+HiroshiAT@users.noreply.github.com>
2023-06-07 16:53:53 -0300

    testeClassQuarto

--- visitanteapp/__init__.py
+++ visitanteapp/__init__.py
@@ -1,1 +1,9 @@
-print('oi')
+class Quarto:
+	def __init__(self, tipo, numero, capacidade):
+		self.tipo=tipo
+		self.numero=numero
+		self.capacidade=capacidade
+quarto=Quarto(1,32,4)
+print(quarto.tipo)
+print(quarto.numero)
+print(quarto.capacidade)
