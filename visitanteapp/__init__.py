415992d8b6e64b7cdb5201d2194280ba442101dd
Gabriel Dreyer <dreyergabriel333@gmail.com>
2023-06-10 16:45:46 -0300

    class quarto na branch main. incorreto.

--- visitanteapp/__init__.py
+++
@@ -1,9 +0,0 @@
-class Quarto:
-	def __init__(self, tipo, numero, capacidade):
-		self.tipo=tipo
-		self.numero=numero
-		self.capacidade=capacidade
-quarto=Quarto(1,32,4)
-print(quarto.tipo)
-print(quarto.numero)
-print(quarto.capacidade)
