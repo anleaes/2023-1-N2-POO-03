b436f7890b1636d3812d73a0e51498da8a7f9d73
JimmyKling <jimmykling528@gmail.com>
2023-06-13 20:26:50 -0300

    Agendamento View

--- apps/agendamento/views.py
+++ apps/agendamento/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
+from .models import Agendamento
+from rest_framework import viewsets
+from .serializers import AgendamentoSerializer
+
+class AgendamentoViewSet(viewsets.ModelViewSet):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer  
