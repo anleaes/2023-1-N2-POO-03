2820d4fbbbfb42192162f261cca6d1f8efb1bfa5
JimmyKling <jimmykling528@gmail.com>
2023-06-13 20:29:42 -0300

    Agendamento teste

--- apps/agendamento/models.py
+++ apps/agendamento/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Agendamento(models.Model):
-    visitante = models.ForeignKey(Visitante, on_delete=models.CASCADE)
-    quarto = models.ForeignKey(Quarto, on_delete=models.CASCADE)
+    #visitante = models.ForeignKey(Visitante, on_delete=models.CASCADE)
+    #quarto = models.ForeignKey(Quarto, on_delete=models.CASCADE)
     data_entrada = models.DateTimeField("Data Entrada")
     data_saida = models.DateTimeField("Data Saída")
 
@@ -14,4 +14,5 @@ class Agendamento(models.Model):
         ordering =['id']
 
     def __str__(self):
-        return self.visitante, self.quarto, self.data_entrada, self.data_saida
+        #return self.visitante, self.quarto, self.data_entrada, self.data_saida
+        return self.data_entrada, self.data_saida
--- apps/agendamento/urls.py
+++ apps/agendamento/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 app_name = 'agendamento'
 
 router = routers.DefaultRouter()
-router.register('agendamento', views.CategoryViewSet, basename='agendamento')
+router.register('agendamento', views.AgendamentoViewSet, basename='agendamento')
 
 urlpatterns = [
     path('', include(router.urls) )
