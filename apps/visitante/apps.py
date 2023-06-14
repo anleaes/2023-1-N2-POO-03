8cdf578a5254210ee810ebdef6fdbb0a94b9828a
Gabriel Dreyer <dreyergabriel333@gmail.com>
2023-06-14 20:46:17 -0300

    classe visitante criada.

--- apps/visitante/apps.py
+++ apps/visitante/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class VisitanteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'visitante'
+    verbose_name = 'Visitante'
--- apps/visitante/models.py
+++ apps/visitante/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Visitante(models.Model):
+    nome = models.TextField("Tipo", max_length=20, default="Não Especificado")
+    cpf = models.TextField("Tipo", max_length=20, default="Não Especificado")
+    telefone = models.TextField("Tipo", max_length=20, default="Não Especificado")
+
+    class Meta:
+        verbose_name = 'Visitante'
+        verbose_name_plural = 'Visitantes'
+        ordering =['id']
+
+    def __str__(self):
+        return self.nome, self.cpf, self.telefone
+
